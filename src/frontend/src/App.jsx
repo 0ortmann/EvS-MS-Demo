acb953a19346cd222b4b661db4e5a1bb5a342315
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2015-12-17 11:30:20 +0100

    Bootstrap frontend

---
+++ src/frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.scss';
+
+export class App extends Component {
+
+    render() {
+        return (
+            <div className='app'>
+                <h3>Hallo Welt!</h3>
+            </div>);
+    }
+}
