65b923b45f9abcac4b1492f834e2a04e61aed561
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2015-12-17 16:18:32 +0100

    Image JSON Api and saving images via post.

--- src/microservice2/src/index.php
+++ src/microservice2/src/index.php
@@ -2,13 +2,17 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+date_default_timezone_set('Europe/Berlin');
+
 use Silex\Provider\FormServiceProvider;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 const MONGO_SERVER = 'mongodb://localhost:27017/evs';
 
@@ -34,7 +38,7 @@ class ImageData {
     public $original_image;
     /** @var string */
     public $operator;
-    /** @var File[] */
+    /** @var \Symfony\Component\HttpFoundation\File\UploadedFile[] */
     public $images;
 }
 
@@ -45,16 +49,40 @@ $imageForm = $formFactory
     ->add('name', TextType::class)
     ->add('original_image', FileType::class)
     ->add('operator', TextType::class)
-//    ->add('images', FileType::class, ['multiple' => true])
+    ->add('images', FileType::class, ['multiple' => true])
     ->getForm();
 
-$app->get('/images/{name}', function($name) use($app, $images) {
-    $image = $images->findOne(['name' => $name]);
+$app->get('/images', function() use($app, $images) {
+    $image = $images->find();
 
-    return 'Hello ' . $app->escape($image['name']);
+    $images = [];
+    while ($next = $image->getNext()) {
+        $images[] = $next;
+    }
+
+    return new JsonResponse($images);
 });
 
-$app->match('/images', function (Request $request) use($images, $imageForm, $gridfs) {
+$app->get('/files/{id}', function($id) use($app, $images, $gridfs) {
+    $file = $gridfs->findOne(['_id' => new MongoId($id)]);
+
+    if ($file === null) {
+        throw new NotFoundHttpException('File not found');
+    }
+
+    return new Response($file->getBytes(), 200, ['Content-type' => $file->file['contentType']]);
+});
+
+$app->get('/images/{name}', function($name) use($app, $images, $gridfs) {
+    $image = $images->findOne(['name' => $name]);
+
+    $cursor = $gridfs->find(['_id' => $image['original_image']]);
+
+    $file = $cursor->getNext();
+    return new Response($file->getBytes(), 200, ['Content-type' => $file->file['contentType']]);
+});
+
+$app->post('/images', function (Request $request) use($images, $imageForm, $gridfs) {
     $imageForm->handleRequest($request);
 
     if (!$imageForm->isValid()) {
@@ -64,12 +92,17 @@ $app->match('/images', function (Request $request) use($images, $imageForm, $gri
     /** @var ImageData $data */
     $data = $imageForm->getData();
 
-    $id = $gridfs->storeFile($data->original_image->getPathname(), ['filename' => $data->original_image->getClientOriginalName(), 'contentType' => 'image/png']);
+    $id = $gridfs->storeFile($data->original_image->getPathname(), ['filename' => $data->original_image->getClientOriginalName(), 'contentType' => $data->original_image->getClientMimeType()]);
 
-    $images->insert(['name' => $data->name, 'operator' => $data->operator, 'original_image' => $id]);
+    $imageFiles = [];
+    foreach ($data->images as $imageFile) {
+        $imageFiles[] = $gridfs->storeFile($imageFile->getPathname(), ['filename' => $imageFile->getClientOriginalName(), 'contentType' => $imageFile->getClientMimeType()]);
+    }
 
-    $data->original_image = $id;
-    return json_encode($data);
+    $insert = ['name' => $data->name, 'operator' => $data->operator, 'original_image' => $id, 'images' => $imageFiles];
+    $images->insert($insert);
+
+    return new JsonResponse($insert);
 });
 
 $app->run();
