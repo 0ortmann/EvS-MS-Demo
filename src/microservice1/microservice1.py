08d7a622c823659578999d5051d784b484d02c42
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2015-12-17 14:53:25 +0100

    server side logic for sending triggering image processing by post, sending processed images to other service.

--- src/microservice1/microservice1.py
+++ src/microservice1/microservice1.py
@@ -2,6 +2,8 @@
 
 from flask import Flask, request, json, Response
 from bildverarbeitung import *
+from requests import post as send
+from werkzeug import secure_filename
 
 '''
 Simple Flask-API for serving requests. API offers stuff for basic image processing.
@@ -13,12 +15,16 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def extractImage(request):
+    ''' Returns a file, if contained in the given request '''
     #print(request.files)
     #print(request.files['file'])
-    return request.files['file']
-
+    file = request.files['file']
+    filename = secure_filename(file.filename)
+    #print(filename)
+    return filename, file
 
 def process(image, operation):
+    ''' Process the given image with the given operation if possible. Unknown operation result in "None"-return. '''
     if (operation == 'roberts_cross'):
         return image.roberts_cross()
     elif (operation == 'sobel'):
@@ -27,24 +33,32 @@ def process(image, operation):
         return image.scharr()
     elif (operation == 'prewitt'):
         return image.prewitt()
-    elif (operation == 'kirsch'):
-        return image.kirsch()
-    elif (operation == 'laplacian'):
-        return image.laplacian()
+    #elif (operation == 'kirsch'):  // kirsch und laplace sind scheiße
+    #    return image.kirsch()
+    #elif (operation == 'laplacian'):
+    #    return image.laplacian()
     else: return None
 
+def sendToStorage(imagename, operation):
+    ''' looks up the images by name and sends them to the persistency layer '''
+    #print(imagename)
+    name, format = imagename.split('.')
 
-
-
+    send('http://localhost:1337/images', files={
+        name + '_combined': open('img/' + operation + '_combined.' + format, 'rb'),
+        name + '_directions': open('img/' + operation + '_directions.' + format, 'rb'),
+        name + '_imag': open('img/' + operation + '_imag.' + format, 'rb'),
+        name + '_magnitudes': open('img/' + operation + '_magnitudes.' + format, 'rb'),
+        name + '_real': open('img/' + operation + '_real.' + format, 'rb')})
+    
 @app.route('/process/<operation>', methods=['POST'])
 def acceptImage(operation):
     ''' Basic image processing on the posted image '''
-    posted = extractImage(request)
-    print(operation)
+    name, posted = extractImage(request)
     image = Image.from_file(posted)
-    process(image, operation)
-    
-    return Response("Hooray", 200)
+    process(image, operation) # stores everything in img/ folder
+    sendToStorage(name, operation)
+    return Response("Success", 200)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
